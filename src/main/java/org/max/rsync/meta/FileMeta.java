8ea31bad599007f50c6cd2f08e4dbce23c9e7fd5
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-07-11 15:16:51 +0300

    Bigger text file added for e-2-e testing + custom serilization added for file metdata.

--- src/main/java/org/max/rsync/meta/FileMeta.java
+++ src/main/java/org/max/rsync/meta/FileMeta.java
@@ -1,6 +1,57 @@
 package org.max.rsync.meta;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.Serial;
+import java.util.ArrayList;
 import java.util.List;
 
-public record FileMeta(List<ChunkMeta> chunkMetas) implements java.io.Serializable {
+public final class FileMeta implements java.io.Externalizable {
+
+    public static final long serialVersionUID = 2026391092388879077L;
+
+    private List<ChunkMeta> chunkMetas;
+
+    public FileMeta(){}
+
+    public FileMeta(List<ChunkMeta> chunkMetas) {
+        this.chunkMetas = chunkMetas;
+    }
+
+    public List<ChunkMeta> chunkMetas() {
+        return chunkMetas;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        System.out.println("CUSTOM serialization used");
+
+        out.writeInt(chunkMetas.size());
+
+        for (ChunkMeta singleMeta : chunkMetas) {
+            out.writeInt(singleMeta.id());
+            out.writeInt(singleMeta.rollingHash());
+            out.writeUTF(singleMeta.strongHash());
+        }
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException {
+        System.out.println("CUSTOM serialization used");
+
+        chunkMetas = new ArrayList<>();
+
+        final int chunksCount = in.readInt();
+
+        for (int i = 0; i < chunksCount; ++i) {
+            int id = in.readInt();
+            int rollingHash = in.readInt();
+            String strongHash = in.readUTF();
+            chunkMetas.add(new ChunkMeta(id, rollingHash, strongHash));
+        }
+    }
+
 }
